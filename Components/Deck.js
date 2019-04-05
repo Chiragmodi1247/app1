5ac168646563d11f88553dfe85d69149fa994ea7
chiragmodi1247 <mchirag1247@gmail.com>
2019-04-06 01:55:34 +0530

    Card moving out

--- Components/Deck.js
+++ Components/Deck.js
@@ -3,6 +3,8 @@ import { View, Animated, Text, PanResponder, Dimensions } from 'react-native';
 import { Button, Card } from 'react-native-elements';
 
 const SCREEN_WIDTH = Dimensions.get("window").width;
+const SWIPE_THRESHOLD = 0.25 * SCREEN_WIDTH;
+const SWIPE_OUT_DURATION = 250;
 
 class Deck extends Component {
 
@@ -15,17 +17,32 @@ class Deck extends Component {
             onPanResponderMove: (event, gesture) => {
                 position.setValue({ x: gesture.dx, y: gesture.dy })
             },
-            onPanResponderRelease: () => {
-                this.resetPosition();
+            onPanResponderRelease: (event, gesture) => {
+                if (gesture.dx > SWIPE_THRESHOLD) {
+                    this.forceSwipeRight();
+                }
+                else if (gesture.dx < -SWIPE_THRESHOLD) {
+
+                }
+                else {
+                    this.resetPosition();
+                }
             }
         });
 
         this.state = { panResponder, position };
     }
 
+    forceSwipeRight() {
+        Animated.timing(this.state.position, {
+            toValue: { x: SCREEN_WIDTH*2 , y: 0 },
+            duration: SWIPE_OUT_DURATION
+        }).start();
+    }
+
     resetPosition() {
-        Animated.spring( this.state.position , {
-            toValue: { x: 0, y: 0}
+        Animated.spring(this.state.position, {
+            toValue: { x: 0, y: 0 }
         }).start();
     }
 
@@ -33,8 +50,8 @@ class Deck extends Component {
 
         const { position } = this.state;
         const rotate = position.x.interpolate({
-            inputRange: [ -SCREEN_WIDTH*2, 0, SCREEN_WIDTH*2],
-            outputRange: ['-120deg' , '0deg' , '120deg']
+            inputRange: [-SCREEN_WIDTH * 2, 0, SCREEN_WIDTH * 2],
+            outputRange: ['-120deg', '0deg', '120deg']
         });
         return {
             ...this.state.position.getLayout(),
