0b1c8e33f7fc7feefe7cd53bb36fe3c95b6048e0
chiragmodi1247 <mchirag1247@gmail.com>
2019-04-06 00:04:32 +0530

    Card styled

--- Components/Deck.js
+++ Components/Deck.js
@@ -1,10 +1,25 @@
 import React, { Component } from 'react';
 import { View, Animated, Text } from 'react-native';
+import { Button, Card } from 'react-native-elements';
 
 class Deck extends Component {
     renderCards() {
         return this.props.data.map(item => {
-            return <Text>{item.text}</Text>;
+            return (
+                <Card
+                    key={item.id}
+                    title={item.text}
+                    image={{ uri: item.uri }}
+                >
+                    <Text style={{marginBottom: 10}}>
+                        I Can Customize card further.
+                    </Text>
+                    <Button
+                    backgroundColor = "#03A9F4"
+                    title = "View Now!"
+                    />
+                </Card>
+            );
         });
     }
     render() {
--- Screens/DeckScreen.js
+++ Screens/DeckScreen.js
@@ -34,7 +34,7 @@ export default DeckScreen;
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center'
+//        alignItems: 'center',
+//        justifyContent: 'center'
     }
 });
