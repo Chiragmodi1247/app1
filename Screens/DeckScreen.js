99dade5aeac9ce9741197e1865bad28b6f637f4f
chiragmodi1247 <mchirag1247@gmail.com>
2019-04-05 23:49:55 +0530

    Navigation is working

--- Screens/DeckScreen.js
+++ Screens/DeckScreen.js
@@ -1,20 +1,30 @@
 import React, { Component } from "react";
-import { 
+import {
     View,
     Text,
     StyleSheet
 } from "react-native";
 import { Actions } from 'react-native-router-flux';
+import Deck from '../Components/Deck';
+
+const DATA = [
+    { id: 1, text: 'Card #1', uri: 'http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-04.jpg' },
+    { id: 2, text: 'Card #2', uri: 'http://www.fluxdigital.co/wp-content/uploads/2015/04/Unsplash.jpg' },
+    { id: 3, text: 'Card #3', uri: 'http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-09.jpg' },
+    { id: 4, text: 'Card #4', uri: 'http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-01.jpg' },
+    { id: 5, text: 'Card #5', uri: 'http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-04.jpg' },
+    { id: 6, text: 'Card #6', uri: 'http://www.fluxdigital.co/wp-content/uploads/2015/04/Unsplash.jpg' },
+    { id: 7, text: 'Card #7', uri: 'http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-09.jpg' },
+    { id: 8, text: 'Card #8', uri: 'http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-01.jpg' },
+];
 
 class DeckScreen extends Component {
     render() {
         return (
             <View style={styles.container}>
-                <Text
-                    onPress={() => Actions.Map()}
-                >
-                DeckScreen
-                </Text>
+                <Deck
+                    data={DATA}
+                />
             </View>
         );
     }
